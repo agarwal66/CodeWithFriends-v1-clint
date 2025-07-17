096d0e4765305a975273e5cadeddc0b368e6433f
Prateek Agarwal <agarwalprateek666@gmail.com>
2025-07-17 15:33:48 +0530

    updated login

--- src/index.js
+++ src/index.js
@@ -1,16 +1,22 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+
 import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+import { ThemeProvider, CssBaseline, createTheme } from '@mui/material';
+
+const chakraTheme = extendTheme({});
+const muiTheme = createTheme({});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
-const theme = extendTheme({}); // Optional, empty theme if you're not customizing
-
 root.render(
   <React.StrictMode>
-    <ChakraProvider theme={theme}>
-      <App />
+    <ChakraProvider theme={chakraTheme}>
+      <ThemeProvider theme={muiTheme}>
+        <CssBaseline />
+        <App />
+      </ThemeProvider>
     </ChakraProvider>
   </React.StrictMode>
 );
